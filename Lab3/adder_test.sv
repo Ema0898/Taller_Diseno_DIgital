721c8ce0e6686f335c79277b3abb72d825770da4
Emmanuelle Aguilar Sánchez <aguisnchez@gmail.com>
2019-03-13 00:12:47 -0600

    Agrega el módulo de banderas y el módulo ALU que integra al resto

--- Lab3/adder_test.sv
+++ Lab3/adder_test.sv
@@ -3,14 +3,13 @@ module adder_test();
 	parameter N = 4;
 
 	logic [N - 1 : 0] a, b, z;
-	logic [3:0] s;
-	logic carry_out, msb_sum;
+	logic [3:0] s, flags;
 	
-	combinational_circuit_module #(N) DUT(a, b, s, z, carry_out, msb_sum);
+	ALU #(N) DUT(a, b, s, z, flags);
 	
 	initial begin
-		#10 a = 'b1011; b = 'b1001; s = 'b1110;
-		#1 $display("a = %b, b = %b, s = %b, z = %b, carry_out = %b, msb = %b", a, b, s, z, carry_out, msb_sum);
+		#10 a = 'b1010; b = 'b1010; s = 'b0111;
+		#1 $display("a = %b, b = %b, s = %b, z = %b, flags = %b", a, b, s, z, flags);
 			
 	end
 
