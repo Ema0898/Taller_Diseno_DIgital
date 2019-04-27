45d0a26507411b8b76c4c3f7fec00e1c97075e02
ema0898 <aguisnchez@gmail.com>
2019-04-27 15:18:47 -0600

    Arregla dibujo de sprie y agrega movimiento manual

--- Lab5/VGA/Painter/PixelGenerator.sv
+++ Lab5/VGA/Painter/PixelGenerator.sv
@@ -1,11 +1,11 @@
-module PixelGenerator(input logic [9:0] posX, posY, pixelX, pixelY, output logic [23:0] RGB);
+module PixelGenerator(input logic [9:0] posX, posY, pixelX, pixelY, input logic [1:0] selec, output logic [23:0] RGB);
 
 	logic [23:0] spriteColor;
 	logic spriteActive;
 	
 	logic [23:0] background = 24'b101010111111111111111011;
 	
-	Sprite sprite(posX, posY, pixelX, pixelY, spriteColor, spriteActive);
+	Sprite sprite(posX, posY, pixelX, pixelY, selec, spriteColor, spriteActive);
 	
 	mux_2_x_1 #(24) mux0(background, spriteColor, spriteActive, RGB);
 	
--- Lab5/VGA/VGA.sv
+++ Lab5/VGA/VGA.sv
@@ -1,6 +1,8 @@
 module VGA (
     input clk,  // Clock FPGA
     input reset, 
+	 input s0,
+	 input s1,
     output vga_hs,
     output vga_vs,
     output logic [7:0] red,
@@ -14,19 +16,23 @@ module VGA (
 	 logic [9:0] posX, posY;
 	 //logic [23:0] RGB = 24'b101010111111111111111011;
 	 logic [23:0] RGB;
+	 logic [1:0] s;
     
     ClkDivisor VGAClkDivisor(clk,clockVGA);
     
     ControllerSync VGASync(clockVGA,~reset,
         vga_hs,vga_vs,hCounter,vCounter,vga_blank_n); 
 
-	 assign posX = 200;
-	 assign posY = 300;
+	 assign posX = 300;
+	 assign posY = 200;
+	 
+	 assign s[1] = ~s1;
+	 assign s[0] = ~s0;
         
     //ControllerPainter VGAPainter(~reset,
       //RGB,blue,green,red);
 		
-	PixelGenerator pixel(posX, posY, hCounter, vCounter, RGB);
+	PixelGenerator pixel(posX, posY, vCounter, hCounter, s, RGB);
 	
 	assign red = RGB[23:16];
 	assign green = RGB[15:8];
